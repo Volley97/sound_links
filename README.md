fec6d128b3fba102756201fa05600c693ed2f095
Volley97 <andrukhserzh@gmail.com>
2019-01-24 14:42:31 +0200

    Made_changes_from_comment

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# sound_links
+# Sound_links
+
 Search links of audiobook
+
+# Install and tuning chromedriver
+	
+	1. Download chromedriver 
+
+		link : 'http://chromedriver.chromium.org/downloads'
+
+	2. Add path to your chromedriver in system PATH:
+
+		export PATH=$PATH:/path/to/driver/chrome-driver
+
+	 * This is done in order not to specify the path to the file manually
--- sound_main.py
+++ sound_main.py
@@ -2,8 +2,8 @@
 from selenium import webdriver
 import time
 
-driver = webdriver.Chrome('D:\Новая папка\chromedriver.exe')
-driver.get('https://www.audioknigi-online.com/after-3.html')
+driver = webdriver.Chrome('\chromedriver.exe')
+driver.get('https://www.audioknigi-online.com')
 time.sleep(5)
 utt = list(map(lambda el:el.get_attribute('src'),driver.find_elements_by_tag_name('audio')))
 print(utt)
